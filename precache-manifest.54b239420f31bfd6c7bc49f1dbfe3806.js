eeb9ca12f2165025b45fecf904fc97fcdb317334
BlackBeard <moisesnt2@gmail.com>
2020-08-25 10:24:45 +0200

    Updates

---
+++ precache-manifest.54b239420f31bfd6c7bc49f1dbfe3806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcb92a8fab92b219660942509e57a89",
+    "url": "/monstersList/index.html"
+  },
+  {
+    "revision": "7613145f7925dcd2cf21",
+    "url": "/monstersList/static/css/main.096a8da2.chunk.css"
+  },
+  {
+    "revision": "397c66361b41de7b72f2",
+    "url": "/monstersList/static/js/2.dcc6d78f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersList/static/js/2.dcc6d78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7613145f7925dcd2cf21",
+    "url": "/monstersList/static/js/main.4851d0ef.chunk.js"
+  },
+  {
+    "revision": "1d683b5f26d47d24e887",
+    "url": "/monstersList/static/js/runtime-main.21257333.js"
+  }
+]);
